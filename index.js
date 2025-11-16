e443d522fa562f9c0efb8534a47ef03bfa6f2d6b
Yasodhar Gubba <yasodhargubba@gmail.com>
2025-11-16 11:29:34 +0530

    working on the drizzle integration

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 import express from 'express'
 const app=express();
 const PORT=process.env.PORT ?? 8000;
+import userRouter from './routes/user.routes.js'
+app.use(express.json());
+
 
 app.get("/",(req,res)=>{
     return res.json({status:'server is up and running'});
 })
+
+app.use('/user',userRouter)
 
 app.listen(PORT,()=>{
 console.log('Server is running on port 8000');
---
+++ routes/user.routes.js
@@ -0,0 +1,66 @@
+import express from 'express';
+import {db} from '../db/index.js';
+import {usersTable} from '../models/index.js'
+import { eq } from 'drizzle-orm';
+import {signupPostRequestBodySchema,loginPostBodySchema} from '../validation/request.validation.js'
+import {hashPasswordWithSalt} from '../utils/hash.js'
+import {getUserByEmail} from '../services/user.service.js'
+import { error } from 'console';
+import jwt from 'jsonwebtoken'
+
+const router = express.Router();
+
+router.post('/signup', async (req, res) => {
+    const validationResult = await signupPostRequestBodySchema.safeParseAsync(req.body);
+
+    if (validationResult.error) {
+        return res.status(400).json({ error: validationResult.error.format() })
+    }
+
+    const { firstname, lastname, email, password } = validationResult.data
+
+    const existingUser=await getUserByEmail(email);
+    
+    if(existingUser) return res.status(400).json({ error: 'user with this email already exists' });
+
+    const {salt,password:hashedPassword} =hashPasswordWithSalt(password)
+
+    const [user] = await db.insert(usersTable)
+        .values({
+            email,
+            firstname,
+            lastname,
+            salt,
+            password: hashedPassword
+        })
+        .returning({ id: usersTable.id });
+
+    return res.status(201).json({ data: { userId: user.id } });
+});
+
+router.post('/login', async (req, res) => {
+    const validationResult = await loginPostBodySchema.safeParseAsync(req.body);
+
+    if (validationResult.error) {
+        return res.status(400).json({ error: validationResult.error })
+    }
+
+    const { email, password } = validationResult.data
+
+    const user = await getUserByEmail(email)   // <-- FIXED
+
+    if (!user) {
+        return res.status(404).json({ error: 'user with this email does not exist' })
+    }
+
+    const { password: hashedPassword } = hashPasswordWithSalt(password, user.salt)
+
+    if (user.password !== hashedPassword) {
+        return res.status(400).json({ error: 'invalid password' })
+    }
+
+    const token = jwt.sign({ id: user.id }, process.env.JWT_SECRET)
+    return res.json({ token })
+});
+
+export default router;
